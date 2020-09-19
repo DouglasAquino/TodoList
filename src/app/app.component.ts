4c13f3b6c07eea11b87b47799ddc3d8f7826809e
DouglasAquino <douglasaquino817@gmail.com>
2020-09-19 11:26:35 -0300

    Alteração

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,14 +16,17 @@ export class AppComponent {
 
   salvar(){
     if(this.editando){
+
       this.editando.descricao=this.descricao;
     }
     else{
       let d = new Todo(this.descricao);
+
       this.abertas.push(d);
     }
-    this.descricao=null;
     this.editando=null;
+    this.descricao=null;
+    
   }
   executar(todo: Todo){
     let indice = this.abertas.indexOf(todo);
@@ -35,9 +38,12 @@ export class AppComponent {
     if(this.editando == todo){
       alert('Impossivel excluir durante a execução!');
     }
+
     else{
       if(confirm('Tarefa ainda aberta, tem certeza que deseja excluir?')){
+
         let a = this.abertas.indexOf(todo);
+
         this.abertas.splice(a,1);
       }
     }
@@ -46,15 +52,18 @@ export class AppComponent {
   excluirConcluidas(todo: Todo){
     if(confirm('Você está prestes a deletar uma tarefa concluida, tem certeza?')){
       let c = this.concluidas.indexOf(todo);
+
       this.concluidas.splice(c,1);
     }
   }
   editar(todo: Todo){
     this.editando=todo;
+
     this.descricao=todo.descricao;
   }
   cancelar(){
     this.editando=null;
     this.descricao=null;
+    
   }
 }
